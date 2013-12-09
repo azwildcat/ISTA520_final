b8f742173912ac47d0743a2ac36fa240c96ea4bf
Nick Gizzi <ngizzi@email.arizona.edu>
2013-12-08 18:52:45 -0700

    added user name, machine path, and host name to this script

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
 # A script that sets up the iRODS environment on the server
 # For more information, check https://pods.iplantcollaborative.org/wiki/display/start/Using+icommands
-from fabric.api import run
+from fabric.api import *
+env.user =  'ngizzi'
+env.host = ['login.hpc.arizona.edu']
 def setup_icommands():
     run("wget -c http://www.iplantcollaborative.org/sites/default/files/irods/icommands.x86_64.tar.bz2")
     run("bunzip2 icommands.x86_64.tar.bz2")
@@ -8,7 +10,7 @@ def setup_icommands():
     #export PATH=~/icommands:${PATH}
     run("echo \"export PATH=~/icommands:'${PATH}'\" >> ~/.bashrc")
     # Setting up Bash autocomplete for icommands
-    run("wget -c https://pods.iplantcollaborative.org/wiki/download/attachments/6720192/i-commands-auto.bash?version=1&modificationDate=1320265052000")
+    run("wget -c https://pods.iplantcollaborative.org/wiki/download/attachments/6720192/i-commands-auto.bash")
     run("mv i-commands-auto.bash .i-commands-auto.bash")
     run("echo source .i-commands-auto.bash >> ~/.bashrc")
     #run("")

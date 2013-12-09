7965e9f20153d33cc923fa5e2b68e799905f7a9e
azwildcat <coder@wildcat.az>
2013-12-08 21:18:31 -0700

    Corrected the script to not expand the PATH macro.

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,14 @@
 # A script that sets up the iRODS environment on the server
 # For more information, check https://pods.iplantcollaborative.org/wiki/display/start/Using+icommands
 from fabric.api import *
-env.user =  'ngizzi'
+env.user =  'ykk'
 env.host = ['login.hpc.arizona.edu']
 def setup_icommands():
     run("wget -c http://www.iplantcollaborative.org/sites/default/files/irods/icommands.x86_64.tar.bz2")
     run("bunzip2 icommands.x86_64.tar.bz2")
     run("tar -xvf icommands.x86_64.tar")
     #export PATH=~/icommands:${PATH}
-    run("echo \"export PATH=~/icommands:'${PATH}'\" >> ~/.bashrc")
+    run("echo export PATH=~/icommands:'${PATH}' >> ~/.bashrc")
     # Setting up Bash autocomplete for icommands
     run("wget -c https://pods.iplantcollaborative.org/wiki/download/attachments/6720192/i-commands-auto.bash")
     run("mv i-commands-auto.bash .i-commands-auto.bash")

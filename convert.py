80d4c03bef5a428afe00bcedcf6875d58f005db5
azwildcat <coder@wildcat.az>
2013-12-09 18:56:38 -0700

    Modified the comments in the header. Commented-out chmod. Added a debug variable.

--- convert.py
+++ convert.py
@@ -1,24 +1,28 @@
-# this script uses the convert function of imagemagik
-# to mass convert color .jpg images to black and white
-# .pgm files and changes the change permission to 755
-# so that the images can be used for swift keypoint
-# extraction.
-# input: < ./ directory containing images >
-# Out put: .pgm file
-#Auther:  Ian Montgomery (ianmonty@email.arizona.edu)
+# This script uses the `convert` utility of Imagemagik
+# to convert color .jpg images to grey scale .pgm files.
+# Input: < path to directory with images >
+# Output: .pgm file
+# Author:  Ian Montgomery (ianmonty@email.arizona.edu)
 
 import os
 import sys
 import multiprocessing
 
+debug = 1 # whether or not to print commands
 
 #print multiprocessing.cpu_count()
 path = sys.argv[1]
 os.chdir(path)
-cmd = 'for x in `ls *.jpg`; do convert $x ${x%.*}.pgm; done'
-print cmd
+#cmd = 'for x in `ls *.jpg`; do convert $x ${x%.*}.pgm; done'
+
+if os.path.isdir('../pgm') == False: 
+    os.mkdir('../pgm')
+cmd = 'for x in `ls *.jpg`; do convert $x ../pgm/${x%.*}.pgm; done'
+if debug: print cmd
 os.system(cmd)
+"""
 cmd = 'for x in `ls *.pgm`; do chmod -c 755 $x; done'
 print cmd
 os.system(cmd)
 os.chdir('..')
+"""

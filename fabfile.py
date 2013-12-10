66b2b19fd9b653db260beaf3c4a91eb39d6ecb70
azwildcat <coder@wildcat.az>
2013-12-10 00:46:39 -0700

    Included a new function for setting up SIFT. For some reason, it doesn't allow to run 'make' (included the error in the comments).

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 # A script that sets up the iRODS environment on the server
 # For more information, check https://pods.iplantcollaborative.org/wiki/display/start/Using+icommands
 from fabric.api import *
-env.user =  'ykk'
+env.user =  'jorgeorodriguez'
 env.host = ['login.hpc.arizona.edu']
 def setup_icommands():
     run("wget -c http://www.iplantcollaborative.org/sites/default/files/irods/icommands.x86_64.tar.bz2")
@@ -15,3 +15,18 @@ def setup_icommands():
     run("echo source .i-commands-auto.bash >> ~/.bashrc")
     #run("")
     #run("")
+
+def setup_sift():
+    run("wget -c http://www.vlfeat.org/download/vlfeat-0.9.17-bin.tar.gz")
+    run("tar -xvf vlfeat-0.9.17-bin.tar.gz")
+    run("rm -f vlfeat-0.9.17-bin.tar.gz")
+    
+    run("cp ~/.bashrc ~/.bashrc_before_sift")
+    run("echo export PATH=~/vlfeat-0.9.17/:'${PATH}' >> ~/.bashrc")
+   
+    # The last command throws "out: make: *** No targets specified and no makefile found.  Stop." error
+    # followed by "Fatal error: run() received nonzero return code 2 while executing!
+    # Requested: make
+    # Executed: /bin/bash -l -c "make"  "
+    #run("cd vlfeat-0.9.17")
+    #run("make")
